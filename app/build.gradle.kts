aad766d2169c2653001c8897440b0a0836bbd839
Leandro Rubiano <j4ckal.r@gmail.com>
2026-02-08 16:31:35 -0500

    Fix Gradle Kotlin DSL and Firebase BOM mismatch

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,8 +3,6 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("com.google.gms.google-services")
     id("com.google.devtools.ksp")
-    id 'com.android.application'
-    id 'com.google.gms.google-services'
 }
 
 android {
@@ -70,6 +68,7 @@ dependencies {
 
     implementation(platform("com.google.firebase:firebase-bom:33.1.1"))
     implementation("com.google.firebase:firebase-auth-ktx")
+    implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.android.gms:play-services-auth:21.2.0")
 
     debugImplementation("androidx.compose.ui:ui-tooling")
@@ -80,7 +79,4 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-
-    implementation platform('com.google.firebase:firebase-bom:34.9.0')
-    implementation 'com.google.firebase:firebase-analytics'
 }
