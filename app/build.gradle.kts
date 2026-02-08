a991e122700d64201a783093f25b65dbb3f26720
J4cKaLLL <j4ckal.r@gmail.com>
2026-02-08 11:03:44 -0500

    Initial Config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("com.google.gms.google-services")
     id("com.google.devtools.ksp")
+    id 'com.android.application'
+    id 'com.google.gms.google-services'
 }
 
 android {
@@ -78,4 +80,7 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+
+    implementation platform('com.google.firebase:firebase-bom:34.9.0')
+    implementation 'com.google.firebase:firebase-analytics'
 }
